eb4ed2848c630e9d3c614c023630d3234da82f65
Gagan Chordia <60386381+gagan-gv@users.noreply.github.com>
2021-03-21 11:24:51 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,3 @@
-function viewTechStack() {
-    document.getElementById("techStack").style.visibility = "visible";
-    document.getElementById("home").style.visibility = "collapse";
-}
-
-function viewHome() {
-    document.getElementById("techStack").style.visibility = "collapse";
-    document.getElementById("home").style.visibility = "visible";
-}
-
 setTimeout(function() {
     $('.loader_bg').fadeToggle();
 }, 2500);
