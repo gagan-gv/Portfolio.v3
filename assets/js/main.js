683b1029832b5712d51a74463962a228397ebe90
Gagan Chordia <60386381+gagan-gv@users.noreply.github.com>
2021-04-26 10:19:42 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,3 @@
 setTimeout(function() {
     $('.loader_bg').fadeToggle();
 }, 2500)
-
-let mode = document.getElementById("mode");
-
-function changeMode() {
-    if (mode.classList.contains('fa-moon-o')) {
-        mode.className = "fa fa-sun-o"
-        document.body.style.background = "url('./assets/images/bgDark.jpg')"
-        document.body.style.backgroundRepeat = 'no-repeat'
-        document.body.style.backgroundSize = 'cover'
-    } else {
-        mode.className = "fa fa-moon-o"
-        document.body.style.background = "url('./assets/images/bg.jpg')"
-        document.body.style.backgroundRepeat = 'no-repeat'
-        document.body.style.backgroundSize = 'cover'
-    }
-}
-
-mode.addEventListener("click", changeMode)
