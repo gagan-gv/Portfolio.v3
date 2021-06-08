2c47ed83ae92e7cf0850c6311bda29317163ace2
Gagan Chordia <chordiagagan@gmail.com>
2021-06-08 15:14:14 +0530

    Integrated the contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,34 @@
 setTimeout(function() {
     $('.loader_bg').fadeToggle();
 }, 2500)
+
+window.addEventListener("load", () => {
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault()
+
+        var name = document.getElementById("name").value;
+        var msg = document.getElementById("msg").value;
+        var body = "From: " + name + " Message: " + msg
+
+        window.open('mailto:chordiagagan@gmail.com?subject="Mailed for reach"&body=' + body)
+            /*var formdata = {
+                "name": name,
+                "email": email,
+                "msg": msg
+            };
+            axios({
+                    method: "post",
+                    url: "https://mailgagan.herokuapp.com/index",
+                    data: formdata,
+                })
+                .then(() => {
+                    alert("Mailed");
+                })
+                .catch(() => {
+                    alert("Not mailed");
+                });*/
+    });
+
+})
